32c56209c504c83afcdcbe049c554f348b8d55e7
nachkontrolle <49277562+nachkontrolle@users.noreply.github.com>
2024-12-16 21:35:11 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PocusNavigation from './components/PocusNavigation';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <PocusNavigation />
+    </div>
+  );
+}
+
+export default App;
